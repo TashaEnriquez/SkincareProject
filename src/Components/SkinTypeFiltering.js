5279d1841a36b4fa752c49d24ad71a59f36aa6f3
Natasha Enriquez <95707854+TashaEnriquez@users.noreply.github.com>
2022-05-02 09:43:33 +0200

    solve conflict

--- src/Components/SkinTypeFiltering.js
+++ src/Components/SkinTypeFiltering.js
@@ -1,3 +1,13 @@
-const SkinTypeFiltering = () => {};
+const SkinTypeFiltering = () => {
+  return (
+    <div>
+      <h3>Skin Type</h3>
+      <p>Oily</p>
+      <p>Dry</p>
+      <p>Combination</p>
+      <p>Normal</p>
+    </div>
+  );
+};
 
 export default SkinTypeFiltering;
