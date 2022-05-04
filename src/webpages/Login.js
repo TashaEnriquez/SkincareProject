cad0749fcccaed7be97f8a7516bf666350170d65
krupabahulya <krupabahulya.y@gmail.com>
2022-05-04 23:05:40 +0200

    signup form, login form are working

--- src/webpages/Login.js
+++ src/webpages/Login.js
@@ -1,17 +1,40 @@
 import '../styles/Login.css';
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { useAuth } from '../context/AuthContext';
+import Loading from '../Loading';
 
 
 const Login = () => {
+    const [{ email, password }, setFormState] = useState({
+    email: '',
+    password: ''
+  });
+  const { isAuthenticated, loading, loginUser } = useAuth();
+
+  const handleChange = e => setFormState(prev => ({ ...prev, [e.target.id]: e.target.value }));
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if (!email || !password) return toast.error('Please fill all the fields');
+    await loginUser({ email, password });
+  };
+  if (isAuthenticated) return <Navigate to='/protected/routinegenerator' replace />;
+  if (loading) return <Loading />;
+
     return (
       <div className="login-form">
-       <form>
+       <form onSubmit={handleSubmit}>
          <h1>Login</h1>
          <div className="content">
              <div className="input-field">
-                 <input type="email" placeholder="Email" autocomplete="nope" />
+                 <input type="email" placeholder="Email" id="email" name="email"
+                 value={email} onChange={handleChange} />
              </div>
              <div className="input-field">
-                 <input type="password" placeholder="Password" autocomplete="new-password" />
+                 <input type="password" placeholder="Password" id="password" name="password"
+                 value={password} onChange={handleChange}  />
             </div>
          </div>
             <div className="action">
